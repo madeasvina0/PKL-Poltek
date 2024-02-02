68e6d66573edd1ae3470e8a07fa9bbf63f5326df
madeasvina0 <madeasvina@gmail.com>
2024-02-02 11:38:46 +0800

    update

--- app/src/main/java/com/example/aplikasimember/RegisterActivity.java
+++ app/src/main/java/com/example/aplikasimember/RegisterActivity.java
@@ -3,12 +3,22 @@ package com.example.aplikasimember;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    String [] item = {"Laki-Laki", "Perempuan"};
+
+    AutoCompleteTextView autoCompleteTextView;
+
+    ArrayAdapter<String> adapterItems;
 
     TextView login;
 
@@ -20,6 +30,19 @@ public class RegisterActivity extends AppCompatActivity {
         login = (TextView) findViewById(R.id.tvRegister);
 
         btnRegister = findViewById(R.id.btnRegister);
+
+        autoCompleteTextView = findViewById(R.id.auto_complete_txt);
+        adapterItems = new ArrayAdapter<String>(this, R.layout.list_gender, item);
+
+        autoCompleteTextView.setAdapter(adapterItems);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+
+            }
+        });
 
 
 
